d900d1ef7d0d40f9c5a84af8f9f81f9d897aae54
alfonso <unclefonso@protonmail.com>
2022-09-20 19:38:37 -0400

    wip vercel deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./data/db.json')
-const middlewares = jsonServer.defaults({"static":"./docs/api"})
+const middlewares = jsonServer.defaults({"static":"./public"})
 
 server.use(middlewares)
 // Add this before server.use(router)
