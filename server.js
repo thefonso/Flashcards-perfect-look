25cb4db9bfa6147dfcb98186ecf349c0a5af6808
alfonso <unclefonso@protonmail.com>
2022-09-20 18:19:36 -0400

    WIP getting json-server on vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./data/db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({"static":"./docs/api"})
 
 server.use(middlewares)
 // Add this before server.use(router)
