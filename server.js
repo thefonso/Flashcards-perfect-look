c49ef3aceccb392751ef8ee74644014f86e9efa0
alfonso <unclefonso@protonmail.com>
2022-09-20 20:43:01 -0400

    wip json server configuring continued

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./data/db.json')
 const middlewares = jsonServer.defaults({"static":"./public"})
+const express = require('express');
+const path = require('path');
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(router)
+server.use('/db', middlewares, router);
+server.use(express.static(path.join(__dirname, 'build')));
+server.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 server.listen(8080, () => {
     console.log('JSON Server is running')
 })
