2098451e9a1ea89d8715f8cfabb39129ecff928e
alfonso <unclefonso@protonmail.com>
2022-09-20 21:37:38 -0400

    wip back end end point json server hell

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./data/db.json')
 const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(router)
+//server.use('/db')
+server.use('/',middlewares,router)
 server.listen(8080, () => {
     console.log('JSON Server is running')
 })
